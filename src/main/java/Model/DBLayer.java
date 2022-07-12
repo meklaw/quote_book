2ae2155ed11b5af9b90b4b824246d3874a8c5a65
Никита Ульянов <nik.ule@yandex.ru>
2022-07-12 07:55:05 +0300

    add database

--- src/main/java/Model/DBLayer.java
+++ src/main/java/Model/DBLayer.java
@@ -1,4 +1,84 @@
 package Model;
 
-public class DBLayer implements ModelLayer{
+import Controller.User;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class DBLayer implements ModelLayer {
+    private static final Connection connection = createConnection();
+
+
+    private static Connection createConnection() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("src/main/resources/DB.properties"));
+            return DriverManager.getConnection(
+                    properties.getProperty("url"),
+                    properties.getProperty("name"),
+                    properties.getProperty("password"));
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void registerPerson(String login, String password, int group) {
+        String updateSql = "INSERT INTO пользователь (логин, хэш_пароля, номер_группы) Values (?, ?, ?)";
+        try {
+            PreparedStatement updatePreparedStatement = connection.prepareStatement(updateSql);
+            updatePreparedStatement.setString(1, login);
+            updatePreparedStatement.setString(2, String.valueOf(password.hashCode()));
+            updatePreparedStatement.setInt(3, group);
+            updatePreparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int authorizePerson(String name, String password) {
+        String selectSql = "SELECT * FROM пользователь WHERE логин = ? AND хэш_пароля = ?";
+        try {
+            PreparedStatement selectPreparedStatement = connection.prepareStatement(selectSql);
+            selectPreparedStatement.setString(1, name);
+            selectPreparedStatement.setString(2, String.valueOf(password.hashCode()));
+            ResultSet rs = selectPreparedStatement.executeQuery();
+            if (rs.next())
+                return rs.getInt("id");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return -1;
+    }
+
+    @Override
+    public void updateUser(int personID) {
+        String selectSql = "SELECT * FROM пользователь WHERE id = ?";
+
+        try {
+            PreparedStatement selectPreparedStatement = connection.prepareStatement(selectSql);
+            selectPreparedStatement.setInt(1, personID);
+            ResultSet rs = selectPreparedStatement.executeQuery();
+            if (rs.next()) {
+                User.id = personID;
+                User.login = rs.getString("логин");
+                User.group = rs.getInt("номер_группы");
+                User.accessLevel = rs.getInt("уровень_доступа");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/Model/ModelLayer.java
+++ src/main/java/Model/ModelLayer.java
@@ -1,4 +1,11 @@
 package Model;
 
-public interface ModelLayer {
+import Controller.User;
+
+public interface ModelLayer extends AutoCloseable{
+    void registerPerson(String login, String password, int group);
+    int authorizePerson(String login, String password);
+    void updateUser(int personID);
+
+
 }
